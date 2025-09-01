13571bdf28a3ad2e658adc4df510f38473b79b0d
User <User@DESKTOP-R7SCS33>
2025-09-01 12:01:39 +0300

    Add project files.

---
+++ pz1/pz1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    system("color E1");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string last_name;
+    cout << "Введіть прізвище: ";
+    getline(cin, last_name);
+
+    string first_name;
+    cout << "Введіть ім'я: ";
+    getline(cin, first_name);
+
+    int age;
+    cout << "Введіть вік: ";
+    cin >> age;
+
+    cin.ignore();
+
+    string faculty;
+    cout << "Введіть факультет: ";
+    getline(cin, faculty);
+
+    string group;
+    cout << "Введіть групу: ";
+    getline(cin, group);
+
+    int course;
+    cout << "Введіть курс: ";
+    cin >> course;
+    cin.ignore();
+
+    cout << "\nВаші дані:" << endl;
+    cout << "Прізвище: " << last_name << endl;
+    cout << "Ім'я: " << first_name << endl;
+    cout << "Вік: " << age << endl;
+    cout << "Факультет: " << faculty << endl;
+    cout << "Група: " << group << endl;
+    cout << "Курс: " << course << endl;
+
+    return 0;
+}
